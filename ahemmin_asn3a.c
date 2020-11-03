f8c7279b942e8a7fe1ba9c626c078853753058d8
Alexander Hemming <alexanderhemming13@Gmail.com>
2020-11-03 00:52:39 -0500

    Add files via upload

---
+++ ahemmin_asn3a.c
@@ -0,0 +1,132 @@
+
+/* CS 2211a 2020 */
+/* Assignment 03 */
+/* Alexander Hemming */
+/* 251033611 */
+/* ahemmin */
+/* 10/19/2020 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+	int array[] = {12, 63, 44, 89, 3, 55, 73, 27, 37, 18, 14, 24, 30, 52, 69};
+	int number_of_bytes = sizeof(array);
+	int number_of_elements = sizeof(array)/sizeof(int);
+	int counter = 0;
+	int counter2 = 0;
+	int stored_element;
+	int return_element;
+	int position_of_return_element;
+	int sum = 0;
+	int j = 0;
+	int k = 0;
+
+printf("\n");
+printf("Size of Array: %lu bytes\n", number_of_bytes);
+printf("Length of Array: %lu elements\n", number_of_elements);
+printf("\n");
+printf("PART 1: \n");
+printf("The values stored into the array are : \n");
+for (int i = 0; i < number_of_elements; i++) {
+	printf("%lu ", array[i]);	
+}
+printf("\n");
+printf("\n");
+printf("PART 2:\n");
+printf("The values stored into the array in reverse are : \n");
+for (int i = number_of_elements-1; i >= 0; i--) {
+	printf("%lu ", array[i]);
+}
+printf("\n");
+printf("\n");
+printf("PART 3:\n");
+printf("The smallest value stored in the array is :\n");
+for (int i = 0; i < number_of_elements; i++) {
+	stored_element = array[i];
+	counter2 = 0;
+	for (int j = 0; j < number_of_elements; j++) {
+		++counter;
+		if (stored_element > array[j]){
+			counter2++;
+		}
+		if (counter2 == 0 & counter > (number_of_elements-1)) {
+			return_element = stored_element;
+			position_of_return_element = i + 1;
+		}		
+	}
+}
+printf("value: %lu ", return_element);
+printf("at the %dth position from the left\n", position_of_return_element);
+printf("\n");
+printf("PART 4:\n");
+printf("The sum (total) value of the array is : ");
+for (int i = 0; i < number_of_elements; i++) {
+	sum = sum + array[i];
+	printf("%lu ", array[i]);
+	if (i < (number_of_elements-1)) {
+		printf("+ ");
+	}
+	if (i == (number_of_elements-1)) {
+		printf(" equals: %lu ", sum);
+	}	
+}
+printf("\n");
+printf("\n");
+printf("PART 5:\n");
+printf("Copy the array into a new array, but in reverse order :\n");
+printf("Original array :\n ");
+for (int i = 0; i < number_of_elements; i++) {
+	printf("%lu ", array[i]);
+}
+printf("\n");
+printf("New (Reversed) array :\n ");
+int copy_array[number_of_elements];
+for (int i = number_of_elements-1; i >= 0; i--) {
+	copy_array[j] = array[i];
+	j++;
+}
+for (int k = 0; k < number_of_elements; k++) {
+	printf("%lu ", copy_array[k]);
+}
+printf("\n");
+printf("\n");
+printf("PART 6:\n");
+printf("Switch the first value in the array with the last value in the array :\n");
+printf("Original array :\n ");
+for (int i = 0; i < number_of_elements; i++) {
+	printf("%lu ", array[i]);
+}
+printf("\n");
+printf("Changed array :\n ");
+printf("%lu ", array[number_of_elements-1]);
+for (int i = 1; i < (number_of_elements-1); i++) {
+	printf("%lu ", array[i]);
+}
+printf("%lu\n", array[0]);
+printf("\n");
+printf("PART 7:\n");
+printf("Sort the array in ascending order :\n");
+printf("Original array :\n ");
+for (int i = 0; i < number_of_elements; i++) {
+	printf("%lu ", array[i]);
+}
+printf("\n");
+printf("Changed array :\n ");
+for (int i = 0; i < number_of_elements; i++) {
+	for (int j = i + 1; j < number_of_elements; j++) {
+		if (array[i] > array[j]) {
+			stored_element = array[i];
+			array[i] = array[j];
+			array[j] = stored_element;
+		}
+	}
+}
+for (int k = 0; k < number_of_elements; k++) {
+	printf("%lu ", array[k]);
+}
+printf("\n");
+printf("\n");
+}
+
---
+++ ahemmin_asn3b.c
@@ -0,0 +1,32 @@
+
+/* CS 2211a 2020 */
+/* Assignment 03 */
+/* Alexander Hemming */
+/* 251033611 */
+/* ahemmin */
+/* 10/19/2020 */
+
+#include<stdio.h>
+
+// The program takes in user input and adds them to the current total value if and only if they are odd.  It is capable of adding/summing all integer values apart from 1 which will reset the total of the sum and 0 that will exit the program.  Additionally the program is unable to handle negative values appropriately and also starts 2 due to modulo. //
+
+int main() {
+int sum, loop_value2, user_inputted_value, indicator; // Defining/declaring variables //
+
+while(1) {	// Repeat loop until user inputs 0 causing program to break out of loop //
+	sum = 0;	// Intializing sum and resetting when user inputs 1 and program loops around again //
+	printf("Input? ");  // Prompt for user input //
+	scanf("%d", &user_inputted_value);   // Taking in user input //
+	if (user_inputted_value == 0) {	   // Determines if user inputted the exit value 0 //
+	       	break;		// Breaks out of program due to user inputting 0 //
+	}
+	for (int loop_value1 = 1; loop_value1 <= user_inputted_value; loop_value1++) { // iterates up to user inputted value //    
+		indicator = 0;		// Establishes/resets default value //
+		for (loop_value2 = 2; loop_value2 <= loop_value1; loop_value2++) // Determines the amount to add to sum //
+			if (loop_value1 % loop_value2 == 0)  indicator++;  // Amount you want to add is in fcat valid (user is trying to add an odd number //
+		if (indicator == 1)  sum = sum + loop_value1;   // Will add to sum if the amount being is valid //
+	}
+	printf("The final total is: %d\n", sum);   // Prints out final sum in a nice format //
+}
+return 0;	// Required output for when program terminates //
+}
